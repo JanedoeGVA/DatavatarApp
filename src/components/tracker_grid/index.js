2cff469641636936d612b2e8be977eb744c5851a
JanedoeGVA <xavier.costa@unige.ch>
2018-12-09 20:11:24 +0100

    modify component stateless, need to test it

--- src/components/tracker_grid/index.js
+++ src/components/tracker_grid/index.js
@@ -4,21 +4,32 @@ import GridView from 'react-native-super-grid';
 import styles from './styles';
 import TrackerGridItem from '../tracker_grid_item';
 
-export default class GridComponent extends React.Component {
-  render() {
-    const { onPressItem, setItemColor, lstTrackers } = this.props;
-    return (
-      <GridView
-        itemDimension={130}
-        items={lstTrackers}
-        style={styles.gridView}
-        renderItem={(item) => (
-          <TrackerGridItem {...{ onPressItem, setItemColor, item }} />
-        )}
-      />
-    );
-  }
-}
+const GridComponent = ({ lstTrackers, onPressItem, setItemColor }) => (
+  <GridView
+    itemDimension={130}
+    items={lstTrackers}
+    style={styles.gridView}
+    renderItem={(item) => (
+      <TrackerGridItem {...{ onPressItem, setItemColor, item }} />
+    )}
+  />
+);
+
+// export default class GridComponent extends React.Component {
+//   render() {
+//     const { onPressItem, setItemColor, lstTrackers } = this.props;
+//     return (
+//       <GridView
+//         itemDimension={130}
+//         items={lstTrackers}
+//         style={styles.gridView}
+//         renderItem={(item) => (
+//           <TrackerGridItem {...{ onPressItem, setItemColor, item }} />
+//         )}
+//       />
+//     );
+//   }
+// }
 
 GridComponent.propTypes = {
   onPressItem: PropTypes.func.isRequired,
